89fc25b5347e6060f8e5757a08a744e22b7fe9ca
natukinemoto <nemotonatuki@yahoo.co.jp>
2020-06-28 23:54:42 +0900

    税率計算V1.1(キャッシュバック機能追加)

--- develop/js/PageShift.js
+++ develop/js/PageShift.js
@@ -17,5 +17,10 @@ document.getElementById("Header_btn_rightChild3").addEventListener("click", func
 },false);
 
 document.getElementById("Header_btn_rightChild4").addEventListener("click", function(){
+    window.location.href = "../html/RateCalculation_v1.1.html";
+},false);
+
+// 準備中(工事中)仮置き画面
+document.getElementById("Header_btn_rightChild5").addEventListener("click", function(){
     window.location.href = "../html/Function.html";
 },false);
